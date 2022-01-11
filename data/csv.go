88e48d18861a0393d68d7339719bd7af5bc21ff3
Luis De Anda <luis.deanda@wizeline.com>
2022-01-11 16:43:54 -0600

    Create data layer

---
+++ data/csv.go
@@ -0,0 +1,31 @@
+package data
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+)
+
+type csvDataSource struct {
+	Collection string
+}
+
+func NewCSVDataSource(csvPath string) csvDataSource {
+	return csvDataSource{Collection: csvPath}
+}
+
+func (ds csvDataSource) ReadCollection() [][]string {
+	file, err := os.Open(ds.Collection)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return records
+}
