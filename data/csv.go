4c1e061ab2b2991168aee6fed501e125dd6ca8f1
Luis De Anda <luis.deanda@wizeline.com>
2022-01-25 10:53:28 -0600

    Add descriptive comments for `csv.go` definitions

--- data/csv.go
+++ data/csv.go
@@ -6,15 +6,20 @@ import (
 	"os"
 )
 
+// csvDataSource represents a data source that reads from a CSV file.
 type csvDataSource struct {
 	// collection is a file path to a CSV file.
 	collection string
 }
 
+// NewCSVDataSource receives a path to a CSV file and returns an instance of csvDataSource.
 func NewCSVDataSource(csvPath string) csvDataSource {
 	return csvDataSource{collection: csvPath}
 }
 
+// ReadCollection reads the CSV file associated with the csvDataSource instance
+// and returns an slice of records.
+// Each record is an slice of strings itself.
 func (ds csvDataSource) ReadCollection() [][]string {
 	file, err := os.Open(ds.collection)
 	if err != nil {
