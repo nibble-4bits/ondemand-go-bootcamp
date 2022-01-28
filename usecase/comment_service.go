6b23225437f5341320f02b3b679e93b98d59d32d
Luis De Anda <luis.deanda@wizeline.com>
2022-01-28 10:04:54 -0600

    Create usecase layer for `Comment` entity

---
+++ usecase/comment_service.go
@@ -0,0 +1,31 @@
+package usecase
+
+import (
+	"fmt"
+
+	"github.com/nibble-4bits/ondemand-go-bootcamp/entity"
+)
+
+type commentService struct {
+	repo CommentRepository
+}
+
+// CommentService is an interface that represents basic CRUD operations
+// that can be executed on a Comment entity
+type CommentService interface {
+	GetByID(id int) (*entity.Comment, error)
+}
+
+// NewCommentService receives a CommentRepository and returns an instance of commentService
+func NewCommentService(r CommentRepository) commentService {
+	return commentService{repo: r}
+}
+
+func (s commentService) GetByID(id int) (*entity.Comment, error) {
+	commentFound, err := s.repo.GetByID(id)
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	return commentFound, nil
+}
